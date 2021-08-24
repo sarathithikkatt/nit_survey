872bf7f1c76b1c7c0b213f42402356058b4c8214
you@_simon <sonuisplaying@gmail.com>
2021-08-24 20:38:01 +0530

    post request works

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:survey_app/logicalKeyboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: null,
+      home: LogicalKeyboard(),
     );
   }
 }
