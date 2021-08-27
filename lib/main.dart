41c2851aeb9189e7cdf043603a40a4c359cc30d3
you@_simon <sonuisplaying@gmail.com>
2021-08-27 21:25:23 +0530

    image should be working

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:survey_app/Screens/activity.dart';
 
 import 'package:survey_app/glabal.dart';
 import 'package:survey_app/Screens/home.dart';
 import 'package:survey_app/Screens/login.dart';
+import 'package:survey_app/logicalKeyboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,11 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData.dark(),
-      home: islogged ? HomePage() : LoginPage(),
-
+      // darkTheme: ThemeData.dark(),
+      home: ActivityScreen(),
+      // islogged ? HomePage() : LoginPage(),
     );
   }
 }
