7d2087ef6f244183ea2f1b30ab3a8023d3d8672e
you@_simon <sonuisplaying@gmail.com>
2021-08-27 15:28:04 +0530

    url update

--- lib/post_data.dart
+++ lib/post_data.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 sendData(String userName) {
   http
       .post(
-        Uri.parse('https://script.google.com/macros/s/AKfycbytANo71KlTicCw8GYNRlAupacnDQN4TGGDp7zKVV0QDm0mdv3S/exec?'),
+        Uri.parse('https://script.google.com/macros/s/AKfycbytANo71KlTicCw8GYNRlAupacnDQN4TGGDp7zKVV0QDm0mdv3S/exec'),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
