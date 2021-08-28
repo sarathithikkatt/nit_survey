fa926ef60f546294b1516dfa5ed1b1a8be2a600c
you@_simon <sonuisplaying@gmail.com>
2021-08-28 16:36:05 +0530

    logic for sensor readings done

--- lib/Screens/activity.dart
+++ lib/Screens/activity.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:math' as math;
 
 class ActivityScreen extends StatefulWidget {
   ActivityScreen({Key? key, this.title}) : super(key: key);
@@ -16,10 +17,15 @@ class _ActivityScreenState extends State<ActivityScreen> {
   static const int _snakeColumns = 20;
   static const double _snakeCellSize = 10.0;
 
+  List<List<double>> _accList = [];
+  List<List<double>> _uAccList = [];
+  List<List<double>> _magList = [];
+  List<List<double>> _gyrList = [];
+
   List<double>? _accelerometerValues = <double>[0, 0, 0];
-  List<double>? _userAccelerometerValues;
-  List<double>? _gyroscopeValues;
-  List<double>? _magnetometerValues;
+  List<double>? _userAccelerometerValues = <double>[0, 0, 0];
+  List<double>? _gyroscopeValues = <double>[0, 0, 0];
+  List<double>? _magnetometerValues = <double>[0, 0, 0];
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
 
   @override
@@ -31,10 +37,10 @@ class _ActivityScreenState extends State<ActivityScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sensor Example'),
+        title: const Text('Activity tracking'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(16.0),
@@ -91,36 +97,51 @@ class _ActivityScreenState extends State<ActivityScreen> {
     _streamSubscriptions.add(
       accelerometerEvents.listen(
         (AccelerometerEvent event) {
-          setState(() {
-            if (event.x - _accelerometerValues![0] > 3) _accelerometerValues = <double>[event.x, event.y, event.z];
-          });
+          if ((event.x - _accelerometerValues![0]).abs() > 3 ||
+              (event.y - _accelerometerValues![1]).abs() > 3 ||
+              (event.z - _accelerometerValues![2]).abs() > 3)
+            setState(() {
+              print(" acc: ${_accList.length} mag: ${_magList.length} uAcc: ${_uAccList.length} gyr: ${_gyrList.length} ");
+              _accelerometerValues = <double>[event.x, event.y, event.z];
+              _accList.add([event.x, event.y, event.z]);
+            });
         },
       ),
     );
     _streamSubscriptions.add(
       gyroscopeEvents.listen(
         (GyroscopeEvent event) {
-          setState(() {
-            _gyroscopeValues = <double>[event.x, event.y, event.z];
-          });
+          if ((event.x - _gyroscopeValues![0]).abs() > 1 || (event.y - _gyroscopeValues![1]).abs() > 1 || (event.z - _gyroscopeValues![2]).abs() > 1)
+            setState(() {
+              _gyroscopeValues = <double>[event.x, event.y, event.z];
+              _gyrList.add([event.x, event.y, event.z]);
+            });
         },
       ),
     );
     _streamSubscriptions.add(
       userAccelerometerEvents.listen(
         (UserAccelerometerEvent event) {
-          setState(() {
-            _userAccelerometerValues = <double>[event.x, event.y, event.z];
-          });
+          if ((event.x - _userAccelerometerValues![0]).abs() > 3 ||
+              (event.y - _userAccelerometerValues![1]).abs() > 3 ||
+              (event.z - _userAccelerometerValues![2]).abs() > 3)
+            setState(() {
+              _userAccelerometerValues = <double>[event.x, event.y, event.z];
+              _uAccList.add([event.x, event.y, event.z]);
+            });
         },
       ),
     );
     _streamSubscriptions.add(
       magnetometerEvents.listen(
         (MagnetometerEvent event) {
-          setState(() {
-            _magnetometerValues = <double>[event.x, event.y, event.z];
-          });
+          if ((event.x - _magnetometerValues![0]).abs() > 3 ||
+              (event.y - _magnetometerValues![1]).abs() > 3 ||
+              (event.z - _magnetometerValues![2]).abs() > 3)
+            setState(() {
+              _magnetometerValues = <double>[event.x, event.y, event.z];
+              _magList.add([event.x, event.y, event.z]);
+            });
         },
       ),
     );
